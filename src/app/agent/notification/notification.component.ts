bd78e0c1c250ae1653378fdbced40beaf2ab3447
siraz <siraz.odbus@gmail.com>
2026-05-18 18:21:21 +0530

    New Mobile Vesrion Created

--- src/app/agent/notification/notification.component.ts
+++ src/app/agent/notification/notification.component.ts
@@ -1,11 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient, HttpResponse } from '@angular/common/http';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { NgbModalConfig, NgbModal, NgbModalRef } from '@ng-bootstrap/ng-bootstrap'; 
+import { NgbModalConfig, NgbModal, NgbModalRef } from '@ng-bootstrap/ng-bootstrap';
 import { Agentnotification } from '../../model/agentnotification';
-import {AgentnotificationService } from '../../services/agentnotification.service';
-import {Constants} from '../../constant/constant' ;
-import {NgbDate, NgbCalendar, NgbDateParserFormatter} from '@ng-bootstrap/ng-bootstrap';
+import { AgentnotificationService } from '../../services/agentnotification.service';
+import { Constants } from '../../constant/constant';
+import { NgbDate, NgbCalendar, NgbDateParserFormatter } from '@ng-bootstrap/ng-bootstrap';
 import { NgxSpinnerService } from "ngx-spinner";
 
 @Component({
@@ -15,15 +15,15 @@ import { NgxSpinnerService } from "ngx-spinner";
 })
 export class NotificationComponent implements OnInit {
 
-
   public formConfirm: FormGroup;
   public searchFrom: FormGroup;
+
   modalReference: NgbModalRef;
   confirmDialogReference: NgbModalRef;
 
-   public isSubmit: boolean;
-  public ModalHeading:any;
-  public ModalBtn:any;
+  public isSubmit: boolean;
+  public ModalHeading: any;
+  public ModalBtn: any;
 
   hoveredDate: NgbDate | null = null;
   fromDate: NgbDate | null;
@@ -34,188 +34,325 @@ export class NotificationComponent implements OnInit {
   pagination: any;
   busoperators: any;
 
+  /* =========================================
+     CUSTOM MOBILE DROPDOWN
+  ========================================= */
+
+  public showDropdown: boolean = false;
+
+  public selectedNotificationCount: any = 10;
+
   constructor(
-    private spinner: NgxSpinnerService ,
-    private http: HttpClient, 
+    private spinner: NgxSpinnerService,
+    private http: HttpClient,
     private fb: FormBuilder,
     private ans: AgentnotificationService,
     private modalService: NgbModal,
     config: NgbModalConfig,
-    private calendar: NgbCalendar, 
+    private calendar: NgbCalendar,
     public formatter: NgbDateParserFormatter
-    )
-    { 
-      config.backdrop = 'static';
-      config.keyboard = false;
-      this.ModalHeading = "View Details";
-      this.fromDate = calendar.getToday();
-      this.toDate = calendar.getToday();
-    }   
-    
+  ) {
+
+    config.backdrop = 'static';
+    config.keyboard = false;
+
+    this.ModalHeading = "View Details";
+
+    this.fromDate = calendar.getToday();
+    this.toDate = calendar.getToday();
+
+  }
 
   ngOnInit(): void {
+
     this.spinner.show();
+
     this.searchFrom = this.fb.group({
       rows_number: Constants.RecordLimit,
-      rangeFromDate:[null],
-      rangeToDate:[null]    
-    })
+      rangeFromDate: [null],
+      rangeToDate: [null]
+    });
+
+    this.selectedNotificationCount = Constants.RecordLimit;
 
     this.search();
-  }
-  
-  OpenModal(content) 
-  {
-    this.modalReference=this.modalService.open(content,{ scrollable: true, size: 'lg' });
+
   }
 
-  page(label:any){
+  /* =========================================
+     CUSTOM DROPDOWN
+  ========================================= */
+
+  public toggleDropdown(): void {
+
+    this.showDropdown = !this.showDropdown;
+
+  }
+
+  public getSelectedText(): string {
+
+    if (this.selectedNotificationCount === 'all') {
+      return 'All Notifications';
+    }
+
+    return this.selectedNotificationCount + ' Notifications';
+
+  }
+
+  public selectNotificationCount(value: any): void {
+
+    this.selectedNotificationCount = value;
+
+    this.searchFrom.patchValue({
+      rows_number: value
+    });
+
+    this.showDropdown = false;
+
+    this.search();
+
+  }
+
+  OpenModal(content) {
+
+    this.modalReference = this.modalService.open(content, {
+      scrollable: true,
+      size: 'lg'
+    });
+
+  }
+
+  page(label: any) {
+
     return label;
-   }
-   search(pageurl="")
-  {      this.spinner.show();
+
+  }
+
+  search(pageurl = "") {
+
+    this.spinner.show();
+
     const data = {
-      rows_number:this.searchFrom.value.rows_number,  
-      rangeFromDate:this.searchFrom.value.rangeFromDate,
-      rangeToDate :this.searchFrom.value.rangeToDate,
-      user_id : localStorage.getItem('USERID'),
+      rows_number: this.searchFrom.value.rows_number,
+      rangeFromDate: this.searchFrom.value.rangeFromDate,
+      rangeToDate: this.searchFrom.value.rangeToDate,
+      user_id: localStorage.getItem('USERID'),
     };
-   
-    if(pageurl!="")
-    {
-      this.ans.notificationpaginationReport(pageurl,data).subscribe(
+
+    if (pageurl != "") {
+
+      this.ans.notificationpaginationReport(pageurl, data).subscribe(
         res => {
-          this.notificationcontent= res.data.data.data;
-          this.pagination= res.data.data;
+
+          this.notificationcontent = res.data.data.data;
+          this.pagination = res.data.data;
+
           this.spinner.hide();
+
         }
       );
+
     }
-    else
-    {
+    else {
+
       this.ans.notificationReport(data).subscribe(
         res => {
-          this.notificationcontent= res.data.data.data;
-          this.pagination= res.data.data;
-          // console.log(res.data.data);
+
+          this.notificationcontent = res.data.data.data;
+          this.pagination = res.data.data;
+
           this.spinner.hide();
+
         }
       );
+
     }
+
   }
 
-  refresh()
-  {
+  refresh() {
+
     this.spinner.show();
+
     this.searchFrom = this.fb.group({
       rows_number: Constants.RecordLimit,
-      rangeFromDate:[null],
-      rangeToDate:[null]    
+      rangeFromDate: [null],
+      rangeToDate: [null]
     });
+
+    this.selectedNotificationCount = Constants.RecordLimit;
+
     this.search();
+
   }
 
-  viewDetails(index)
-  {
+  viewDetails(index) {
+
     this.notificationcontentRecord = this.notificationcontent[index];
+
   }
 
   formatDate(date) {
-    var d = new Date(date),
-        month = '' + (d.getMonth() + 1),
-        day = '' + d.getDate(),
-        year = d.getFullYear();
 
-    if (month.length < 2) 
-        month = '0' + month;
-    if (day.length < 2) 
-        day = '0' + day;
+    var d = new Date(date),
+      month = '' + (d.getMonth() + 1),
+      day = '' + d.getDate(),
+      year = d.getFullYear();
+
+    if (month.length < 2)
+      month = '0' + month;
+
+    if (day.length < 2)
+      day = '0' + day;
 
     return [year, month, day].join('-');
+
   }
+
   onDateSelection(date: NgbDate) {
+
     if (!this.fromDate && !this.toDate) {
+
       this.searchFrom.controls.rangeFromDate.setValue(date);
+
       this.fromDate = date;
-    } else if (this.fromDate && !this.toDate && date && date.after(this.fromDate)) {
-      this.toDate = date;
-      this.searchFrom.controls.rangeToDate.setValue(date);
-    } else {
-      this.toDate = null;
-      this.fromDate = date;
-      this.searchFrom.controls.rangeFromDate.setValue(date);
+
     }
+    else if (this.fromDate && !this.toDate && date && date.after(this.fromDate)) {
+
+      this.toDate = date;
+
+      this.searchFrom.controls.rangeToDate.setValue(date);
+
+    }
+    else {
+
+      this.toDate = null;
+
+      this.fromDate = date;
+
+      this.searchFrom.controls.rangeFromDate.setValue(date);
+
+    }
+
   }
 
   validateInput(currentValue: NgbDate | null, input: string): NgbDate | null {
+
     const parsed = this.formatter.parse(input);
-    return parsed && this.calendar.isValid(NgbDate.from(parsed)) ? NgbDate.from(parsed) : currentValue;
+
+    return parsed && this.calendar.isValid(NgbDate.from(parsed))
+      ? NgbDate.from(parsed)
+      : currentValue;
+
   }
+
   isHovered(date: NgbDate) {
-    return this.fromDate && !this.toDate && this.hoveredDate && date.after(this.fromDate) && date.before(this.hoveredDate);
+
+    return this.fromDate &&
+      !this.toDate &&
+      this.hoveredDate &&
+      date.after(this.fromDate) &&
+      date.before(this.hoveredDate);
+
   }
 
   isInside(date: NgbDate) {
-    return this.toDate && date.after(this.fromDate) && date.before(this.toDate);
+
+    return this.toDate &&
+      date.after(this.fromDate) &&
+      date.before(this.toDate);
+
   }
 
   isRange(date: NgbDate) {
-    return date.equals(this.fromDate) || (this.toDate && date.equals(this.toDate)) || this.isInside(date) || this.isHovered(date);
+
+    return date.equals(this.fromDate) ||
+      (this.toDate && date.equals(this.toDate)) ||
+      this.isInside(date) ||
+      this.isHovered(date);
+
   }
 
-  // Pagination helper methods
+  /* =========================================
+     PAGINATION
+  ========================================= */
+
   isPageVisible(label: any): boolean {
-    // Show First, Previous, Next, Last indicators (these are not numeric)
+
     if (label === '&laquo;' || label === '&raquo;') {
-      return false; // Hide arrow indicators
+      return false;
     }
-    
-    // Show pages 1, 2, 3
+
     if (label === '1' || label === '2' || label === '3') {
       return true;
     }
-    
-    // Show current page if it's higher than 3
-    if (parseInt(label) === this.pagination.current_page && parseInt(label) > 3) {
+
+    if (
+      parseInt(label) === this.pagination.current_page &&
+      parseInt(label) > 3
+    ) {
       return true;
     }
-    
+
     return false;
+
   }
 
   getPageLabel(label: any): string {
-    // Return clean page numbers without arrow HTML entities
+
     if (label === '&laquo;' || label === '&raquo;') {
       return '';
     }
+
     return label;
+
   }
 
-  // Pagination button click handlers
   onFirstPage() {
-    if (this.pagination.current_page !== 1 && this.pagination.links.first_page_url) {
+
+    if (
+      this.pagination.current_page !== 1 &&
+      this.pagination.links.first_page_url
+    ) {
+
       this.search(this.pagination.links.first_page_url);
+
     }
+
   }
 
   onPreviousPage() {
+
     if (this.pagination.links.prev_page_url) {
+
       this.search(this.pagination.links.prev_page_url);
+
     }
+
   }
 
   onNextPage() {
+
     if (this.pagination.links.next_page_url) {
+
       this.search(this.pagination.links.next_page_url);
+
     }
+
   }
 
   onLastPage() {
-    if (this.pagination.current_page !== this.pagination.links.last_page && this.pagination.links.last_page_url) {
+
+    if (
+      this.pagination.current_page !== this.pagination.links.last_page &&
+      this.pagination.links.last_page_url
+    ) {
+
       this.search(this.pagination.links.last_page_url);
+
     }
+
   }
 
 }
-
-
